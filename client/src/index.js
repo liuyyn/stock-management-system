bb63022165579a09c1b607fe1cf62e42eec077e2
Yu Yun Liu <yu.yun.liu@mail.mcgill.ca>
2020-08-29 17:31:41 -0400

    wrapped the App component inside a Provider and Router component.

--- client/src/index.js
+++ client/src/index.js
@@ -1,11 +1,15 @@
 import React from "react";
 import ReactDOM from "react-dom";
-import "./index.css";
 import App from "./App";
+import { BrowserRouter as Router } from "react-router-dom";
+import { Provider } from "react-redux"; // binds react and reduc together
+import store from "./components/redux/store";
 
 ReactDOM.render(
-  <React.StrictMode>
-    <App />
-  </React.StrictMode>,
+  <Provider store={store}>
+    <Router>
+      <App />
+    </Router>
+  </Provider>,
   document.getElementById("root")
 );
